c8919ca285511487865a5d6202c229090a474796
Iva Havranová <havranova.i@seznam.cz>
2023-04-03 23:44:01 +0200

    Vady odstraněny, funguje to

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const updateCounter = (number) => {
   const valueElm = document.querySelector('.counter__value');
-  valueElm.value = String(number).padStart(2, '0');
+  valueElm.textContent = String(number).padStart(2, '0');
 };
 
-let value = 0;
+let value = 50;
 const btnUp = document.querySelector('.counter__up');
 const btnDown = document.querySelector('.counter__down');
 
 btnUp.addEventListener('click', () => {
   if (value < 99) {
-    value + 1;
+    value += 1;
     updateCounter(value);
   }
 });
 
 btnDown.addEventListener('click', () => {
   if (value > 0) {
-    value - 1;
+    value -= 1;
     updateCounter(value);
   }
 });
